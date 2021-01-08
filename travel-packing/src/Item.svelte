d0a310a226af4cfabef694a5d777373a8bdbb3af
Dominic Tassio <dominic.tassio@gmail.com>
2021-01-08 00:29:14 -0600

    Create Item.svelte and implement Item component | Listing 4.7

---
+++ travel-packing/src/Item.svelte
@@ -0,0 +1,54 @@
+<script>
+  import { blurOnKey } from "./util";
+
+  export let item;
+
+  let editing = false;
+</script>
+
+<style>
+  button {
+    background-color: transparent;
+    border: none;
+  }
+
+  input[type="checkbox"] {
+    --size: 24px;
+    height: var(--size);
+    width: var(--size);
+  }
+
+  input[type="text"] {
+    border: 1px solid lightgray;
+  }
+
+  li {
+    display: flex;
+    align-items: center;
+  }
+
+  .packed-true {
+    color: gray;
+    text-decoration: line-through;
+  }
+
+  span {
+    margin: 0 10px;
+  }
+</style>
+
+<li>
+  <input type="checkbox" bind:checked={item.packed} />
+  {#if editing}
+    <input
+      type="text"
+      bind:value={item.name}
+      on:blur={() => (editing = false)}
+      on:keydown={blurOnKey} />
+  {:else}
+    <span
+      class="packed-{item.packed}"
+      on:click={() => (editing = true)}>{item.name}</span>
+  {/if}
+  <button class="icon">&#x1F5D1;</button>
+</li>
