5937e48a3b038f2d633099ae59af2054040b58da
Dominic Tassio <dominic.tassio@gmail.com>
2021-01-07 21:13:48 -0600

    Implement loan calculator based on loan amount, interest rate, and years | listing 2.5

--- loan/src/App.svelte
+++ loan/src/App.svelte
@@ -1,30 +1,28 @@
 <script>
-	export let name;
+  let interestRate = 3;
+  let loanAmount = 200_000;
+  let years = 30;
+  const MONTHS_PER_YEAR = 12;
+
+  $: months = years * MONTHS_PER_YEAR;
+  $: monthlyInterestRate = interestRate / 100 / MONTHS_PER_YEAR;
+  $: numerator = loanAmount * monthlyInterestRate;
+  $: denominator = 1 - (1 + monthlyInterestRate) ** -months;
+  $: payment =
+    !loanAmount || !years
+      ? 0
+      : interestRate
+      ? numerator / denominator
+      : loanAmount / months;
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<label for="loan">Loan Amount</label>
+<input id="loan" type="number" bind:value={loanAmount} />
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+<label for="interest">Interest Rate</label>
+<input id="interest" type="number" bind:value={interestRate} />
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+<label for="years">Years</label>
+<input id="years" type="number" bind:value={years} />
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+<div>Monthly Payment: ${payment.toFixed(2)}</div>
